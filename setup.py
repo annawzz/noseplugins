ae74828105503eeed025a317d2b0f82d96a7ef1e
annawzz <annawzz@quintagroup.org>
2017-06-07 16:20:47 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+
+from setuptools import setup
+
+version = '0.10'
+
+setup(name='noseplugins',
+      version=version,
+      description='',
+      long_description=open("README.md").read(),
+      classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python"],
+      keywords='web services',
+      author='Quintagroup, Ltd.',
+      author_email='info@quintagroup.com',
+      url='https://github.com/openprocurement/noseplugins',
+      license='Apache License 2.0',
+      zip_safe = False,
+      entry_points = {
+        'todo': [ 'todo = nose_todo_plugin:TodoWarningPlugin' ]
+      },
+     )
