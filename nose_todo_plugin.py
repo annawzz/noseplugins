df961ae012f617b8f52e31d5af846f94f438baa4
annawzz <annawzz@quintagroup.org>
2017-06-12 12:55:15 +0300

    Prettify code

--- nose_todo_plugin.py
+++ nose_todo_plugin.py
@@ -1,12 +1,13 @@
 """This plugin installs a TODO warning class for the TodoWarningException.
 When TodoWarningException is raised, the exception will be logged
-in the todo attribute of the result, detailed reason will be logged in warning_list,
-'W' or 'Warning' (verbose) will be output, and the exception will not be counted as an error
-or failure.
+in the todo attribute of the result, detailed reason will be logged in warnings,
+'W' or 'Warning' (verbose) will be output, and the assertion exception will not be counted
+as an error or failure.
 """
-from nose.plugins.errorclass import ErrorClass, ErrorClassPlugin
-from functools import wraps
 import warnings
+from functools import wraps
+from nose.plugins.errorclass import ErrorClass, ErrorClassPlugin
+
 
 class TodoWarningException(Exception):
     """ Custom exception class for TodoWarningPlugin.
@@ -15,7 +16,6 @@ class TodoWarningException(Exception):
     def __init__(self, reason, error):
         self.reason = reason
         self.error = error
-    pass
 
 
 class TodoWarningPlugin(ErrorClassPlugin):
@@ -28,13 +28,13 @@ class TodoWarningPlugin(ErrorClassPlugin):
     """
     name = 'todo'
     todo = ErrorClass(TodoWarningException, label='Warning', isfailure=False)
-    warnings_list = []
+    warnings = []
 
     def formatError(self, test, err):
         """ Customize formatError plugin method.
             Add error details to warnings list to be used in finslize method.
         """
-        self.warnings_list.append((err[1].reason, err[1].error))
+        self.warnings.append((err[1].reason, err[1].error))
         ec, ev, tb = err
         return (ec, ev, tb)
 
@@ -44,7 +44,8 @@ class TodoWarningPlugin(ErrorClassPlugin):
         """
         warnings.formatwarning = custom_formatwarning
         for i in range(len(result.todo)):
-            warnings.warn('{}; error {} (in {})\n'.format(self.warnings_list[i][0], self.warnings_list[i][1], result.todo[i][0]))
+            # warnings output after test results
+            warnings.warn('{}; error {} (in {})\n'.format(self.warnings[i][0], self.warnings[i][1], result.todo[i][0]))
 
 
 def custom_formatwarning(msg, *a):
